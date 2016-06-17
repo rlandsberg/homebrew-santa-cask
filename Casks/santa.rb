ae89022be5e19ab4c3b2b76409b08dee7c7166d0
rlandsberg <richard.p.landsberg@gmail.com>
2016-06-17 14:27:30 +1000

    I may be an idiot

--- Casks/santa.rb
+++ Casks/santa.rb
@@ -1,6 +1,6 @@
 cask 'santa' do
   version '0.9.11'
-  sha256 :no_check
+  sha256 '561d4124f9ce146fb15f4b20af80a6d0b4c9fef24b2b6cb39cbb1878d51ea55e'
 
   url "https://github.com/google/santa/releases/download/#{version}/santa-#{version}.dmg"
   name 'Santa'
